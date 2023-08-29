1385ddd4443a7772a916a205d0ce623aed07d264
anishazahan <anishazahan13@gmail.com>
2023-08-29 18:29:02 +0600

    fix some issue

--- src/pages/project/index.js
+++ src/pages/project/index.js
@@ -6,6 +6,7 @@ import Image from "next/image";
 import Link from "next/link";
 import UpdateProject from "./UpdateProject";
 import RootLayout from "@/components/layout/RootLayout";
+import Projects from "@/components/Projects/Projects";
 
 const Project = () => {
   const { data, isLoading, isError, error } = useGetProjectQuery();
@@ -22,24 +23,28 @@ const Project = () => {
   };
 
   return (
-    <div className="custom-container my-24">
-      {data?.data?.map((item, index) => {
-        return (
-          <div key={index} className="">
-            <Image width={400} height={400} src={item.img} alt="" />
-            {/* Pass the id of the project to the UpdateProject component */}
-            <Link href={`/project/UpdateProject?id=${item._id}`}>
-              <button className="border p-3 border-black">Update</button>
-            </Link>
-            <button
-              className="border p-3 border-red-500"
-              onClick={() => handleDeleteProject(item._id)}
-            >
-              Delete
-            </button>
-          </div>
-        );
-      })}
+    // <div className="custom-container my-24">
+    //   {data?.data?.map((item, index) => {
+    //     return (
+    //       <div key={index} className="">
+    //         <Image width={400} height={400} src={item.img} alt="" />
+    //         {/* Pass the id of the project to the UpdateProject component */}
+    //         <Link href={`/project/UpdateProject?id=${item._id}`}>
+    //           <button className="border p-3 border-black">Update</button>
+    //         </Link>
+    //         <button
+    //           className="border p-3 border-red-500"
+    //           onClick={() => handleDeleteProject(item._id)}
+    //         >
+    //           Delete
+    //         </button>
+    //       </div>
+    //     );
+    //   })}
+    // </div>
+
+    <div className="mt-20">
+      <Projects></Projects>
     </div>
   );
 };
