a92f95d7604d0e656dd218433fe1e24730aacb6f
anishazahan <anishazahan13@gmail.com>
2023-08-29 20:10:58 +0600

    animation add in banner section

--- src/components/Home/HomeBanner.jsx
+++ src/components/Home/HomeBanner.jsx
@@ -31,7 +31,12 @@ const HomeBanner = () => {
           <h2 className="dark:text-gray-200 font-semibold mb-3">
             Hey, I am Anisha Zahan Hashi{" "}
           </h2>
-          <h2 className="text-5xl dark:text-gray-200 font-bold mx-auto text-center pb-6">
+          <h2
+            data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="200"
+            className="text-5xl dark:text-gray-200 font-bold mx-auto text-center pb-6"
+          >
             Expert <span className="text-primary">Frontend </span> Developer
           </h2>
           <p className="dark:text-gray-200 text-center mx-auto font-medium max-w-2xl pb-4">
@@ -57,24 +62,36 @@ const HomeBanner = () => {
 
           <div className="flex justify-center items-center space-x-2">
             <Link
+              data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-delay="300"
               href="https://www.facebook.com/anishazahan13?mibextid=ZbWKwL"
               className="text-2xl text-primary border p-2 border-primary border-opacity-60  dark:bg-black/70 rounded-full"
             >
               <BsFacebook></BsFacebook>
             </Link>
             <Link
+              data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-delay="400"
               href="https://join.skype.com/invite/wml21guEKXrn"
               className="text-2xl text-primary border p-2 border-primary border-opacity-60  dark:bg-black/70 rounded-full"
             >
               <AiFillSkype></AiFillSkype>
             </Link>
             <Link
+              data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-delay="500"
               href="anishazahan13@gmail.com"
               className="text-2xl text-primary border p-2 border-primary border-opacity-60  dark:bg-black/70 rounded-full"
             >
               <SiGmail></SiGmail>
             </Link>
             <Link
+              data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-delay="600"
               href="https://www.linkedin.com/in/anisha-zahan/"
               className="text-2xl text-primary border p-2 border-primary border-opacity-60  dark:bg-black/70 rounded-full"
             >
